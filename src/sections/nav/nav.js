0674c74a2361ca5d23a638719cfcc4f3cfe486df
moksh chadha <mokshchadha@mokshs-MacBook-Pro.local>
2022-09-10 22:14:27 +0530

    made nav bar render selected

--- src/sections/nav/nav.js
+++ src/sections/nav/nav.js
@@ -1,13 +1,57 @@
-import React from "react";
+import React, { useState } from "react";
 import "./style.css";
 
+const NAV_LINKS = {
+  ABOUT: "About Me",
+  EXP: "Experience",
+  PROJECTS: "My Projects",
+  CONTACT: "Contact Me",
+};
+
 export const Nav = () => {
+  const [selected, setSelected] = useState("");
+
+  function handleClick(e) {
+    setSelected(e.target.innerText);
+  }
+
+  console.log(selected);
   return (
     <div id="nav">
-      <a href="#about">About</a>
-      <a href="#experience">Experience</a>
-      <a href="#projects">Projects</a>
-      <a href="#contact">Contact</a>
+      <a
+        href="#about"
+        style={getStyle(selected, NAV_LINKS.ABOUT)}
+        onClick={handleClick}
+      >
+        {NAV_LINKS.ABOUT}
+      </a>
+      <a
+        href="#experience"
+        style={getStyle(selected, NAV_LINKS.EXP)}
+        onClick={handleClick}
+      >
+        {NAV_LINKS.EXP}
+      </a>
+      <a
+        href="#projects"
+        style={getStyle(selected, NAV_LINKS.PROJECTS)}
+        onClick={handleClick}
+      >
+        {NAV_LINKS.PROJECTS}
+      </a>
+      <a
+        href="#contact"
+        style={getStyle(selected, NAV_LINKS.CONTACT)}
+        onClick={handleClick}
+      >
+        {NAV_LINKS.CONTACT}
+      </a>
     </div>
   );
 };
+
+function getStyle(selectedValue, valueOfLink) {
+  return selectedValue === valueOfLink
+    ? { backgroundColor: "black", color: "#93E9BE" }
+    : {};
+}
